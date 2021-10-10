781669ca1e354ea083f31436fd9a9d1848d16889
Anggie Sanchez <anggie.sanchez@o365.unab.edu.co>
2021-10-09 20:44:07 -0500

    Se agrega una pagina en JavaScript para registrar compra de un producto

--- js/saludar.js
+++ js/saludar.js
@@ -1,5 +1,34 @@
-alert("Hola mundo segunda forma")
+// alert("Hola mundo segunda forma")
 
 console.log("Hola mundo desde consola");
 
 document.write("<h1>Hola mundo desde JavaScript</h1 >");
+
+// Tipo de datos: String, Number, Boolean;
+
+var nombre = `Anggie`;
+let correo = 'angnatalisan@hotmail.com';
+let mayor = 18;
+
+let edad = 18;
+let altura = 1.81;
+let extranjero = false;
+
+let mensaje = "Hola, soy "+nombre+" mi correo es "+correo+" tengo "+edad+" años";
+
+nombre = prompt("Escriba su nombre...")
+correo = prompt("Escriba su correo...")
+edad = parseInt(prompt("Escriba su edad..."))
+altura = parseFloat(prompt("Escriba su altura..."))
+
+let mensaje2 = `Hola, soy ${nombre} mi correo es ${correo} tengo ${edad} años`;
+
+if (edad>mayor) {
+    mensaje2 += `, soy mayor de edad`;      
+    }else if(edad<mayor){
+        mensaje2 += `, soy menor de edad`;
+    }else{
+        mensaje2 += `, tengo 18`;        
+}
+document.write("<h2>"+mensaje2+"</h2>");
+
